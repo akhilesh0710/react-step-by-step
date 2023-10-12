5baeaf523f5c95c3e9305675e9ad4386bd7b5962
akhilesh0710 <giriakh0710@gmail.com>
2023-10-12 23:43:50 +0530

    Initial commit: Created REACT app my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HelloWorld from './HelloWorld'
+import Greeting from './Greeting'
+
+const App = () => {
+  return (
+    <div>
+      <HelloWorld/>
+      <Greeting name="Akhilesh"/>
+    </div>
+  )
+}
+
+export default App
