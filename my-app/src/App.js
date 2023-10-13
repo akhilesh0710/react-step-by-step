7070e010f9fb7ce267ed2d426c88975bf34d24ff
Akhilesh GIRI <giriakh0710@gmail.com>
2023-10-13 16:02:48 +0530

    testing the code not upading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HelloWorld from './HelloWorld'
 import Greeting from './Greeting'
+import ParentComponent from './BasicPropPassing/ParentComponent'
 
 const App = () => {
   return (
     <div>
       <HelloWorld/>
       <Greeting name="Akhilesh"/>
+      <ParentComponent/>
     </div>
   )
 }
---
+++ my-app/src/BasicPropPassing/ParentComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChildComponent from './ChildComponent'
+
+const ParentComponent = () => {
+  return (
+    <div>
+         
+        <ChildComponent name = "Ravi"/>
+    </div>
+  )
+}
+
+export default ParentComponent
